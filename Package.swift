7d15bf1d3b22873a452e442bdb20a34355505ee3
Jonathan Guthrie <jono@perfect.org>
2017-01-05 10:37:25 -0500

    removed http server dep as it’s already included in sessions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
     name: "Perfect-Session-MySQL-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", majorVersion: 0, minor: 0),
     ]
 )
