ad3e3d980c2c06ddf6b35166f92ce7819c4dd647
Jonathan Guthrie <jono@perfect.org>
2017-10-04 12:05:46 -0400

    updated for swift 4 / perfect 3

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 22:10:45 +0000
+// Date: 2017-10-04 15:52:50 +0000
 import PackageDescription
 let package = Package(
-    name: "Perfect-Session-MySQL-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-    ]
+	name: "Perfect-Session-MySQL-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+	]
 )
--- Sources/Handlers.swift
+++ Sources/Handlers.swift
@@ -10,7 +10,6 @@
 import PerfectLib
 import PerfectHTTP
 import PerfectSession
-import TurnstileCrypto
 
 public class WebHandlers {
 	/* =================================================================================================================
@@ -18,17 +17,13 @@ public class WebHandlers {
 	================================================================================================================= */
 	open static func indexHandlerGet(request: HTTPRequest, _ response: HTTPResponse) {
 
-		let rand = URandom()
-
-		request.session.data[rand.secureToken] = rand.secureToken
-//		print(request.session.data)
 		var dump = ""
 		do {
-			dump = try request.session.data.jsonEncodedString()
+			dump = (try request.session?.data.jsonEncodedString()) ?? ""
 		} catch {
 			dump = "\(error)"
 		}
-		var body = "<p>Your Session ID is: <code>\(request.session.token)</code></p><p>Session data: <code>\(dump)</code></p>"
+		var body = "<p>Your Session ID is: <code>\(request.session?.token ?? "")</code></p><p>Session data: <code>\(dump)</code></p>"
 		body += "<p><a href=\"/withcsrf\">CSRF Test Form</a></p>"
 		body += "<p><a href=\"/nocsrf\">No CSRF Test Form</a></p>"
 
@@ -45,7 +40,7 @@ public class WebHandlers {
 	open static func CORSHandlerGet(request: HTTPRequest, _ response: HTTPResponse) {
 
 		response.addHeader(.contentType, value: "application/json")
-		try? response.setBody(json: ["Success":"CORS Request"])
+		let _ = try? response.setBody(json: ["Success":"CORS Request"])
 		response.completed()
 
 	}
@@ -57,7 +52,7 @@ public class WebHandlers {
 	================================================================================================================= */
 	open static func formNoCSRF(request: HTTPRequest, _ response: HTTPResponse) {
 
-		var body = "<p>Your Session ID is: <code>\(request.session.token)</code></p><form method=\"POST\" action=\"?\" enctype=\"multipart/form-data\">"
+		var body = "<p>Your Session ID is: <code>\(request.session?.token ?? "")</code></p><form method=\"POST\" action=\"?\" enctype=\"multipart/form-data\">"
 		body += "<p>No CSRF Form</p>"
 		body += "<p>NOTE: You should get a failed request because there is no CSRF</p>"
 		body += "<p><input type=\"text\" name=\"testing\" value=\"testing123\"></p>"
@@ -72,8 +67,8 @@ public class WebHandlers {
 	formWithCSRF
 	================================================================================================================= */
 	open static func formWithCSRF(request: HTTPRequest, _ response: HTTPResponse) {
-		let t = request.session.data["csrf"] as? String ?? ""
-		var body = "<p>Your Session ID is: <code>\(request.session.token)</code></p><form method=\"POST\" action=\"?\" enctype=\"multipart/form-data\">"
+		let t = request.session?.data["csrf"] as? String ?? ""
+		var body = "<p>Your Session ID is: <code>\(request.session?.token ?? "")</code></p><form method=\"POST\" action=\"?\" enctype=\"multipart/form-data\">"
 		body += "<p>CSRF Form</p>"
 		body += "<p><input type=\"text\" name=\"testing\" value=\"testing123\"></p>"
 		body += "<p><input type=\"text\" name=\"_csrf\" value=\"\(t)\"></p>"
@@ -89,7 +84,7 @@ public class WebHandlers {
 	================================================================================================================= */
 	open static func formReceive(request: HTTPRequest, _ response: HTTPResponse) {
 		//		print("in formReceive")
-		var body = "<p>Your Session ID is: <code>\(request.session.token)</code></p>"
+		var body = "<p>Your Session ID is: <code>\(request.session?.token ?? "")</code></p>"
 		body += "<p>CSRF Test response</p>"
 		body += "<p>Params: \(request.postParams)</p>"
 		response.setBody(string: header+body+footer)
